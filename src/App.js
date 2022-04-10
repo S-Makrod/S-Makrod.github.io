a1dd1a8328429c116be88de73abaffd249c5877a
Saad Makrod <saad.makrod@gmail.com>
2022-04-10 16:28:02 -0400

    Completed the styling and content

--- src/App.js
+++ src/App.js
@@ -11,8 +11,6 @@ import Projects from './components/Projects'
 import Experience from './components/Experience'
 import Footer from './components/Footer'
 
-// todo styling and colouring
-// todo need content put dummy stuff for now
 const App = () => {
     AOS.init({once: true})
     return (
